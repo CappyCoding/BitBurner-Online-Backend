139d0e4d3b5a97665069ff4c06451fb5e9789db5
MatthiasVandeCasteele <97677167+CappyCoding@users.noreply.github.com>
2022-01-14 14:40:30 +0100

    Post request working

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ app.get("/userdata/get/", async (req, res) => {
 
 app.post("/userdata/send", async (req, res) => {
 
-    if (!req.body.serverName || !req.body.serverPassword || !req.body.userMoney || !req.body.hackingSkill || req.body.isOnline) {
-        res.status(400).send("Bad request: missing id, serverName, serverPassword, userMoney, hackingSkill or isOnline");
+    if (!req.body.serverName || !req.body.serverPassword || !req.body.userMoney || !req.body.hackingSkill || !req.body.isOnline) {
+        res.status(400).send("Bad request: serverName, serverPassword, userMoney, hackingSkill or isOnline");
         return;
     }
 
@@ -70,29 +70,34 @@ app.post("/userdata/send", async (req, res) => {
 
         await client.connect();
         const collection = client.db(dbName).collection(collectionName);
+        let count = collection.estimatedDocumentCount();
 
-        // Validatio for duplicates
+        let serverCount = "server-" + count;
+
+        let newUser = {
+            serverCount: {
+                serverName: req.body.serverName,
+                serverPassword: req.body.serverPassword,
+                userMoney: req.body.userMoney,
+                hackingSkill: req.body.hackingSkill,
+                isOnline: req.body.isOnline
+            }
+        };
+
+        // Validation for duplicates
         const userExists = await collection.findOne({
             serverName: req.body.serverName
         });
+
         if (userExists) {
-            res.status(400).send("Bad request: user already exists with id: " + req.body.id);
+            res.status(400).send("Bad request: user already exists with serverName: " + req.body.serverName);
             return;
         }
 
-        let newUser = {
-            id: ObjectId(req.params.id),
-            serverName: req.body.serverName,
-            serverPassword: req.body.serverPassword,
-            userMoney: req.body.userMoney,
-            hackingSkill: req.body.hackingSkill,
-            isOnline: req.body.isOnline
-        };
-
         // Insert into database
         let insertResult = await collection.insertOne(newUser);
-        res.send(201).send(`User succesfully created with id: ${req.body.id} & serverName: ${req.body.serverName}`);
-        return
+        res.send(201).send(`User succesfully created with serverName: ${req.body.serverName}`);
+        return;
     } catch (error) {
         console.log(error);
         res.status(500).send({
