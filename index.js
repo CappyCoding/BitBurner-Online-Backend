4c15caf708e61499e0acaaea49b50e69497713aa
MatthiasVandeCasteele <97677167+CappyCoding@users.noreply.github.com>
2022-01-14 12:00:45 +0100

    test

--- index.js
+++ index.js
@@ -28,59 +28,66 @@ app.get("/", (req, res) => {
     res.status(300).redirect("/api-info.html");
 });
 
-// app.get("/userdata/get/:id", async (req, res) => {
-//     try {
-//         await client.connect();
-//         const collection = client.db(dbName).collection(collectionName);
-//         const matchID = {
-//             _id: ObjectId(req.params.id)
-//         }
-//         const found = await collection.findOne(matchID);
-//         if (found) {
-//             res.status(200).send(found);
-//             return
-//         } else {
-//             res.status(400).send(`User data not found with id ${req.body.id}`);
-//         }
-//     } catch (error) {
-//         res.status(500).send("error, something went wrong: " + error);
-//     } finally {
-//         await client.close();
-//     }
-// });
+app.get("/userdata/get/", async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection(collectionName);
+        // const matchID = {
+        //     _id: ObjectId(req.params.id)
+        // }
+        const allUserData = await col.find({}).toArray();
 
-// app.post("/userdata/send", async (req, res) => {
-//     try {
-//         await client.connect();
-//         const collection = client.db(dbName).collection(collectionName);
+        console.log(allUserData);
 
-//         const match = await collection.findOne({
-//             serverName: req.body.serverName
-//         });
+        res.status(200).send(allUserData);
 
-//         if (match) {
-//             res.status(400).send("Bad request: Server Name already exists");
-//             return
-//         } else {
+        // const found = await collection.findOne(matchID);
+        // if (found) {
+        //     res.status(200).send(found);
+        //     return
+        // } else {
+        //     res.status(400).send(`User data not found with id ${req.body.id}`);
+        // }
+    } catch (error) {
+        res.status(500).send("error, something went wrong: " + error);
+    } finally {
+        await client.close();
+    }
+});
 
-//             let userData = {
-//                 serverName: req.body.serverName,
-//                 serverPassword: await bcrypt.hash(req.body.password, 10),
-//                 userMoney: req.body.userMoney,
-//                 hackingSkill: req.body.hackingSkill,
-//                 isOnline: req.body.isOnline,
-//             }
+app.post("/userdata/send", async (req, res) => {
+    try {
+        await client.connect();
+        const collection = client.db(dbName).collection(collectionName);
 
-//             let insertUser = await collection.insertOne(userData);
-//             res.status(201).send("User has been succesfully created");
-//             return
-//         }
-//     } catch (error) {
-//         res.status(500).send("error, something went wrong: " + error);
-//     } finally {
-//         await client.close();
-//     }
-// });
+        const match = await collection.findOne({
+            serverName: req.body.serverName
+        });
+
+        if (match) {
+            res.status(400).send("Bad request: Server Name already exists");
+            return
+        } else {
+
+            let userData = {
+                serverName: req.body.serverName,
+                serverPassword: await bcrypt.hash(req.body.password, 10),
+                userMoney: req.body.userMoney,
+                hackingSkill: req.body.hackingSkill,
+                isOnline: req.body.isOnline,
+            }
+
+            let insertUser = await collection.insertOne(userData);
+            res.status(201).send("User has been succesfully created");
+            return
+        }
+    } catch (error) {
+        res.status(500).send("error, something went wrong: " + error);
+    } finally {
+        await client.close();
+    }
+});
 
 app.listen(port, () => {
     console.log(`API running at at http://localhost:${port}`)
