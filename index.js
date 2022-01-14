402fc20e3c9876f547c427a35697fe668505c71b
MatthiasVandeCasteele <97677167+CappyCoding@users.noreply.github.com>
2022-01-14 11:42:08 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const {
     MongoClient,
     ObjectId
 } = require("mongodb");
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const port = process.env.PORT || 1337;
-require('dotenv').config();
+
 const client = new MongoClient(process.env.FINAL_URL);
+const app = express();
+const port = process.env.PORT || 1337;
 const dbName = "Bitburner-Online";
 const collectionName = "userData";
 
@@ -27,59 +28,59 @@ app.get("/", (req, res) => {
     res.status(300).redirect("/api-info.html");
 });
 
-app.get("/userdata/get/:id", async (req, res) => {
-    try {
-        await client.connect();
-        const collection = client.db(dbName).collection(collectionName);
-        const matchID = {
-            _id: ObjectId(req.params.id)
-        }
-        const found = await collection.findOne(matchID);
-        if (found) {
-            res.status(200).send(found);
-            return
-        } else {
-            res.status(400).send(`User data not found with id ${req.body.id}`);
-        }
-    } catch (error) {
-        res.status(500).send("error, something went wrong: " + error);
-    } finally {
-        await client.close();
-    }
-});
+// app.get("/userdata/get/:id", async (req, res) => {
+//     try {
+//         await client.connect();
+//         const collection = client.db(dbName).collection(collectionName);
+//         const matchID = {
+//             _id: ObjectId(req.params.id)
+//         }
+//         const found = await collection.findOne(matchID);
+//         if (found) {
+//             res.status(200).send(found);
+//             return
+//         } else {
+//             res.status(400).send(`User data not found with id ${req.body.id}`);
+//         }
+//     } catch (error) {
+//         res.status(500).send("error, something went wrong: " + error);
+//     } finally {
+//         await client.close();
+//     }
+// });
 
-app.post("/userdata/send", async (req, res) => {
-    try {
-        await client.connect();
-        const collection = client.db(dbName).collection(collectionName);
+// app.post("/userdata/send", async (req, res) => {
+//     try {
+//         await client.connect();
+//         const collection = client.db(dbName).collection(collectionName);
 
-        const match = await collection.findOne({
-            serverName: req.body.serverName
-        });
+//         const match = await collection.findOne({
+//             serverName: req.body.serverName
+//         });
 
-        if (match) {
-            res.status(400).send("Bad request: Server Name already exists");
-            return
-        } else {
+//         if (match) {
+//             res.status(400).send("Bad request: Server Name already exists");
+//             return
+//         } else {
 
-            let userData = {
-                serverName: req.body.serverName,
-                serverPassword: await bcrypt.hash(req.body.password, 10),
-                userMoney: req.body.userMoney,
-                hackingSkill: req.body.hackingSkill,
-                isOnline: req.body.isOnline,
-            }
+//             let userData = {
+//                 serverName: req.body.serverName,
+//                 serverPassword: await bcrypt.hash(req.body.password, 10),
+//                 userMoney: req.body.userMoney,
+//                 hackingSkill: req.body.hackingSkill,
+//                 isOnline: req.body.isOnline,
+//             }
 
-            let insertUser = await collection.insertOne(userData);
-            res.status(201).send("User has been succesfully created");
-            return
-        }
-    } catch (error) {
-        res.status(500).send("error, something went wrong: " + error);
-    } finally {
-        await client.close();
-    }
-});
+//             let insertUser = await collection.insertOne(userData);
+//             res.status(201).send("User has been succesfully created");
+//             return
+//         }
+//     } catch (error) {
+//         res.status(500).send("error, something went wrong: " + error);
+//     } finally {
+//         await client.close();
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`API running at at http://localhost:${port}`)
